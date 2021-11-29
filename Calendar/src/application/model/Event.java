3fa3033a4c98a97bb87d4cb23c5e60059809a398
tdy379 <91216794+tdy379@users.noreply.github.com>
2021-11-28 23:52:40 -0600

    Update Event.java

--- Calendar/src/application/model/Event.java
+++ Calendar/src/application/model/Event.java
@@ -1,5 +1,5 @@
 package application.model;
-
+//Group 3 EasyEvent project
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
@@ -9,12 +9,16 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+//Event class creates Event objects and List of Events, as well as order events by time and date useing the Comparators.
 public class Event{
+	//String varibales day, description, time, notes and List of Events are created.
 	protected String day, description, time, notes;
 	protected List<Event> events = new ArrayList<Event>();
+	//This constructo
 	public Event(List<Event> events) {
 		this.events = events;
 	}
+	//This constructor creates an Event object with given variables.
 	public Event(String day, String description, String time, String notes) {
 		super();
 		this.day = day;
@@ -22,6 +26,8 @@ public class Event{
 		this.time = time;
 		this.notes = notes;
 	}
+	//The loadEvents method reads line by line data from individual user files named <user>.txt and 
+	//parses the lines into varibles day, time, description, to create event objects and add them to the List of Event objects events.
 	public void loadEvents(String user) throws FileNotFoundException {
 		String fLine;
 		String []data;
@@ -44,10 +50,12 @@ public class Event{
 		}
 		IN.close();
 	}
+	//toString method prints the correct format for our event list day, time, description: <description>, notes: <notes>.
 	@Override
 	public String toString() {
 		return day+", "+time + ", description: " + description+ ", notes:" + notes+" \n";
 	}
+	//Comparator sorts our information by soonest time:
 	public static Comparator<Event> compare= new Comparator<Event>() {
 		@Override
 		public int compare(Event o1, Event o2) {
@@ -58,12 +66,15 @@ public class Event{
 			}
 		}
 	};
+	//Comparator sorts our information by soonest day:
 	public static Comparator<Event> compareDates = new Comparator<Event>() {
 		@Override
 		public int compare(Event o1, Event o2) {
 			return o1.getDay().compareTo(o2.getDay());
 		}
 	};
+	//Getters and Setters for the varibales: 
+	//day, time, description, and notes:
 	public String getDay() {
 		return day;
 	}
